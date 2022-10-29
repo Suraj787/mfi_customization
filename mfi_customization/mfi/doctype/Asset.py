d6a204ac72dad74445546a7fb7687348565a8b4c
Hidayat ali <63574705+hidayatmanusiya@users.noreply.github.com>
2022-10-29 15:46:57 +0530

    make task on PM call interval

--- mfi_customization/mfi/doctype/Asset.py
+++ mfi_customization/mfi/doctype/Asset.py
@@ -27,4 +27,31 @@ def on_update(doc, method):
     #updating location in Serial No doctype with Asset location change.
  	frappe.db.set_value('Asset Serial No',doc.serial_no,'location',doc.location)
  	
+	
+def make_task_on_PM_call_interval(doc, method):
+    project = frappe.get_value('Project', {'name': doc.project}, 'status')
+    if project == "Open":
+        if doc.status == "Submitted":
+            task_doc = frappe.new_doc('Task')
+            task_doc.subject = "PM Call Interval"
+            task_doc.customer = frappe.get_value(
+                'Project', {'name': doc.project}, 'customer')
+            task_doc.asset = doc.name
+            task_doc.location = doc.location
+            task_doc.type_of_call = "PM"
+            task_doc.issue_type = "Preventive"
+            task_doc.failure_date_and_time = datetime.today()
+            task_doc.raise_by_contact = frappe.get_value(
+                'Customer', {'name': task_doc.customer}, 'customer_name')
+            task_doc.project = doc.project
+            task_doc.status = "Open"
+            task_doc.serial_no = doc.serial_no
+            task_doc.area = doc.area
+            task_doc.sub_location_area = doc.sub_location_area
+            task_doc.machine_location = doc.machine_location
+            task_doc.completed_by = doc.technician
+            task_doc.save()
+            print("Task created for project", doc.project)
+            print("Asset", doc.name)	
+	
     	
