65f8bb67c1f31678cb73ff42189ddd31ce179bb8
suraj787 <suraj@bizmap.in>
2022-11-02 14:25:32 +0000

    docs and jsons

---
+++ mfi_customization/mfi/report/asset_history/asset_history.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, bizmap technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Asset History"] = {
+	"filters": [
+
+	]
+};
