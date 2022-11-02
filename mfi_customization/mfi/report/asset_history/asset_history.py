6a5dea0a8189831256cb914d01173e4fdac9a5c6
Hidayat ali <107534037+hidayatbizmap@users.noreply.github.com>
2022-11-02 19:07:20 +0530

    Asset History Report

---
+++ mfi_customization/mfi/report/asset_history/asset_history.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2022, bizmap technologies and contributors
+# For license information, please see license.txt
+
+import frappe
+
+
+def execute(filters=None):
+    conditions, filters = get_conditions(filters)
+    columns = get_columns(filters)
+    data = get_data(conditions, filters)
+
+    return columns, data
+
+
+def get_data(conditions, filters):
+
+    item = frappe.db.sql("""select mr.name,mr.reading_date,mr.asset,mr.project,mr.machine_type,mr.colour_reading,mr.black_and_white_reading,mr.total,
+						mrt.item_code,mrt.item_name,mrt.item_group,mrt.yield,mrt.total_reading,mrt.percentage_yield
+                        from `tabMachine Reading` mr
+                        LEFT Join `tabAsset Item Child Table` mrt on mrt.parent = mr.name
+                        where mrt.idx > 0 """)
+
+    return item
+
+
+def get_conditions(filters):
+    conditions = ""
+    # if filters.get("mr.name"): conditions += " and mr.name = %(name)s"
+
+    return conditions, filters
+
+
+def get_columns(filters):
+
+    return [
+
+        {
+            "label": ("Name"),
+            "fieldname": "mr.name",
+            "fieldtype": "Link",
+            "options": "Machine Reading",
+            "width": 200
+        },
+        {
+            "label": ("Reading Date"),
+            "fieldname": "mr.reading_date",
+            "width": 200
+        },
+        {
+            "label": ("Asset"),
+            "fieldname": "mr.asset",
+            "width": 200
+        },
+        {
+            "label": ("Project"),
+            "fieldname": "mr.project",
+            "width": 200
+        },
+        {
+            "label": ("Machine Type"),
+            "fieldname": "mr.machine_type",
+            "width": 200
+        },
+        {
+            "label": ("Colour Reading"),
+            "fieldname": "mr.colour_reading",
+            "width": 100
+        },
+        {
+            "label": ("Black And White Reading"),
+            "fieldname": "mr.black_and_white_reading",
+            "width": 100
+        },
+        {
+            "label": ("Total"),
+            "fieldname": "mr.total",
+            "width": 100
+        },
+        {
+            "label": ("Item Code"),
+            "fieldname": "mrt.item_code",
+            "width": 150
+        },
+        {
+            "label": ("Item Name"),
+            "fieldname": "mrt.item_name",
+            "width": 150
+        },
+        {
+            "label": ("Item Group"),
+            "fieldname": "mrt.item_group",
+            "width": 150
+        },
+        {
+            "label": ("Yield"),
+            "fieldname": "mrt.yield",
+            "width": 100
+        },
+        {
+            "label": ("Total Reading"),
+            "fieldname": "mrt.total_reading",
+            "width": 100
+        },
+        {
+            "label": ("Percentage Yield"),
+            "fieldname": "mrt.percentage_yield",
+            "width": 100
+        },
+
+
+
+    ]
