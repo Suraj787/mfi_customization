8705580ab21a77f14e5b514341dfecaf4289bd6a
Hidayat ali <107534037+hidayatbizmap@users.noreply.github.com>
2022-11-05 16:22:26 +0530

    Update hooks.py

--- mfi_customization/hooks.py
+++ mfi_customization/hooks.py
@@ -117,7 +117,8 @@ doc_events = {
     },
     "Issue":{
         "validate":"mfi_customization.mfi.doctype.issue.validate",
-        "on_change":"mfi_customization.mfi.doctype.issue.on_change"
+        "on_change":"mfi_customization.mfi.doctype.issue.on_change",
+        "after_insert":"mfi_customization.mfi.doctype.issue.after_insert"
     },
     "Material Request":{
         #"on_change":"mfi_customization.mfi.doctype.material_request.set_item_from_material_req",
