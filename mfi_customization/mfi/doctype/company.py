2f70bdd58d1c79b945a5a5a82253c4854aed420e
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2022-11-26 14:01:11 +0530

    feat: add toner support email field on company

--- mfi_customization/mfi/doctype/company.py
+++ mfi_customization/mfi/doctype/company.py
@@ -5,3 +5,4 @@ from frappe.utils import validate_email_address
 
 def validate_support_email(doc, method):
     validate_email_address(doc.support_email, True)
+    validate_email_address(doc.toner_support_email, True)
