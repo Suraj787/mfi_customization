9d72a8848e89a47c930912eb1d58d62e6db89c84
Hidayat ali <107534037+hidayatbizmap@users.noreply.github.com>
2022-12-14 14:56:40 +0530

    Update issue.js

--- mfi_customization/public/js/issue.js
+++ mfi_customization/public/js/issue.js
@@ -523,26 +523,26 @@ frappe.ui.form.on("Asset Readings", "date", function(frm, cdt, cdn) {
 
 
 frappe.ui.form.on('Issue', {
-    asset_name: function(frm) {
-        frappe.call({
-            method: 'mfi_customization.mfi.doctype.issue.asset_name_item',
-            args: {
-                item_code: cur_frm.doc.item_code
-            },
-            callback: function(r) {
-                console.log(r.message);
-                cur_frm.set_query("toner_type", function(doc) {
-                    return {
-                        "filters": [
-                            ['Item', 'name', 'in', r.message]
-                        ]
-                    };
-                });
+    item_code: function(frm) {
+        if (cur.frm.doc.item_code) {
+            frappe.call({
+                method: 'mfi_customization.mfi.doctype.issue.asset_name_item',
+                args: {
+                    item_code: cur_frm.doc.item_code
+                },
+                callback: function(r) {
 
-            }
+                    cur_frm.set_query("toner_type", function(doc) {
+                        return {
+                            "filters": [
+                                ['Item', 'name', 'in', r.message]
+                            ]
+                        };
+                    });
 
-        });
-
+                }
+            });
+        }
     }
 });
 
@@ -556,10 +556,13 @@ frappe.ui.form.on('Issue', {
                     user: frappe.session.user_email
                 },
                 callback: function(r) {
-                    console.log(r.message);
-			if (r.message) {
-                    		frm.set_value("customer", r.message[0]);
-			}
+
+                    if (r.message) {
+                        console.log(r.message[0]);
+                        frm.set_value("customer", r.message);
+                    }
+
+
                 }
             });
         }
