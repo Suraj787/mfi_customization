18994d5762b13a689badf081073e14ae8b93cc5f
Hidayat ali <107534037+hidayatbizmap@users.noreply.github.com>
2022-12-24 12:18:15 +0530

    Update Asset.py

--- mfi_customization/mfi/doctype/Asset.py
+++ mfi_customization/mfi/doctype/Asset.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta, date
 from dateutil import rrule
 from dateutil.relativedelta import relativedelta
 
-
 def after_insert(doc, method):
     if len(frappe.get_all('Asset Serial No', {"serial_no": doc.serial_no}, ['name', 'asset'])) > 0:
         for d in frappe.get_all('Asset Serial No', {"serial_no": doc.serial_no}, ['name', 'asset']):
@@ -34,32 +33,6 @@ def on_update(doc, method):
     frappe.db.set_value('Asset Serial No', doc.serial_no,
                         'location', doc.location)
 
-
-# def make_task_on_PM_call_interval(doc, method):
-#     project = frappe.get_value('Project', {'name': doc.project}, 'status')
-#     if project == "Open":
-#         if doc.status == "Submitted":
-#             task_doc = frappe.new_doc('Task')
-#             task_doc.subject = "PM Call Interval"
-#             task_doc.customer = frappe.get_value(
-#                 'Project', {'name': doc.project}, 'customer')
-#             task_doc.asset = doc.name
-#             task_doc.location = doc.location
-#             task_doc.type_of_call = "PM"
-#             task_doc.issue_type = "Preventive"
-#             task_doc.failure_date_and_time = datetime.today()
-#             task_doc.raise_by_contact = frappe.get_value(
-#                 'Customer', {'name': task_doc.customer}, 'customer_name')
-#             task_doc.project = doc.project
-#             task_doc.status = "Open"
-#             task_doc.serial_no = doc.serial_no
-#             task_doc.area = doc.area
-#             task_doc.sub_location_area = doc.sub_location_area
-#             task_doc.machine_location = doc.machine_location
-#             task_doc.completed_by = doc.technician
-#             task_doc.save()
-#             print("Task created for project", doc.project)
-#             print("Asset", doc.name)
 
 @frappe.whitelist(allow_guest=True)
 def make_task_on_PM_call_interval(doc, method):
@@ -103,6 +76,7 @@ def date_pm_cycle(pm_cycle,project):
     yearlylist=[]
     quarterlylist =[]
     half_yearlist =[]
+    
     endate=str(expected_end_date)
     endate_strp =datetime. strptime(endate, "%Y-%m-%d")
     endateformating = datetime(endate_strp.year,endate_strp.month,endate_strp.day)
@@ -132,4 +106,4 @@ def date_pm_cycle(pm_cycle,project):
           half_yearlist.append(invoce_startformating.date())
           invoce_startformating += add_half_year_Month
     
-
+    return monthlylist,yearlylist,quarterlylist,half_yearlist,bymonthlylist
