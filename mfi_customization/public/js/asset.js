b78afa421aa487587b11abbacd5adf032e08631e
Hidayat ali <107534037+hidayatbizmap@users.noreply.github.com>
2022-12-24 12:19:06 +0530

    Update asset.js

--- mfi_customization/public/js/asset.js
+++ mfi_customization/public/js/asset.js
@@ -1,26 +1,26 @@
 frappe.ui.form.on('Asset', {
 
-   item_code(frm){
-      frappe.model.with_doc("Item",frm.doc.item_code,function(){
-       var itemschild_data = frappe.model.get_doc("Item",frm.doc.item_code)
-       if(itemschild_data.items){
-	  frm.clear_table('items');
-	  $.each(itemschild_data.items,
-	  function(index,row){
-	     var d = frm.add_child('items');
-	     d.item_code = row.item_code
-	     d.item_name = row.item_name
-	     d.item_group = row.item_group;
-	     frm.refresh_field("items")
-	          })
-	   
-             }
+    item_code(frm) {
+        frappe.model.with_doc("Item", frm.doc.item_code, function() {
+            var itemschild_data = frappe.model.get_doc("Item", frm.doc.item_code)
+            if (itemschild_data.items) {
+                frm.clear_table('items');
+                $.each(itemschild_data.items,
+                    function(index, row) {
+                        var d = frm.add_child('items');
+                        d.item_code = row.item_code
+                        d.item_name = row.item_name
+                        d.item_group = row.item_group;
+                        frm.refresh_field("items")
+                    })
+
+            }
 
         })
 
     },
 
-	pm_cycle: function(frm) {
+    pm_cycle: function(frm) {
             frappe.call({
                 method: 'mfi_customization.mfi.doctype.Asset.date_pm_cycle',
                 args: {
@@ -111,5 +111,4 @@ frappe.ui.form.on('Asset', {
         
     }
 
-});
-	
+})
