b9ba948eaccd70579568e7a92f43fcad037ebb48
hrishikesh-bizmap <hrishikesh.karale@bizmap.in>
2023-04-04 15:04:05 +0530

    error throw if current < last

--- mfi_customization/mfi/doctype/task.py
+++ mfi_customization/mfi/doctype/task.py
@@ -526,7 +526,7 @@ def validate_reading(doc):
     if len(curr)>0 and len(last)>0:
         print(f'\n\n\n\n\n122{curr},{last}\n\n\n\n\n')
         # if doc.issue_type != 'Error message':
-        if int(last[-1])>=int(curr[0]) and int(last[-1])>0 and int(curr[0])>0:
+        if int(last[0])>=int(curr[0]) and int(last[-1])>0 and int(curr[0])>0:
             frappe.throw("Current Reading Must be Greater than Last Reading")
 
     if len(curr_date)>0 and len(last_date)>0:
