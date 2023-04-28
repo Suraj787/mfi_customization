8b26ede05fcedb27ea2d5b4b9fed30ae6887119c
suraj787 <suraj@bizmap.in>
2023-04-28 22:56:44 +0000

    ggg

--- mfi_customization/mfi/doctype/task.py
+++ mfi_customization/mfi/doctype/task.py
@@ -36,7 +36,7 @@ def validate(doc,method):
 		fltr={"project":doc.project,"asset":doc.asset,"reading_date":("<=",last_reading)}
 		mr_all = frappe.get_all("Machine Reading",filters=fltr,fields=["name","reading_date","asset","black_and_white_reading","colour_reading","total","machine_type"],limit=4,order_by="reading_date desc,name desc")
 		for d in range(len(mr_all)-1):
-			if len(mr_all)>0 and mr_all[d]['total']!= l[0]:
+			if len(mr_all)>0 and mr_all[d]['total']!= l[0] and int(mr_all[d]['total'])>0:
 				print(f"\n\n\n\n\nttttt,{int(mr_all[d]['total'])}\n\n\n\n\n")
 				print(f"\n\n\n\n\nttttt+++++++++11111111,{int(mr_all[d+1]['total'])}\n\n\n\n\n")
 				doc.append("last_readings", {
