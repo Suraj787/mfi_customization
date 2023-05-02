2ecadad2aec400190d6a878aec987e68cab4f8d3
suraj787 <suraj@bizmap.in>
2023-05-02 10:39:27 +0000

    user perm

--- mfi_customization/mfi/doctype/employee/employee.py
+++ mfi_customization/mfi/doctype/employee/employee.py
@@ -123,6 +123,79 @@ def get_type_of_call(doc,method):
 
                 # Save the user document
             user.save()
+    if doc.designation == 'Call Coordinator':
+        if 'CM' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'CM'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'CM'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'Ins' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'Ins'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'Ins'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'Kyocera Major incident' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'Kyocera Major incident'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'Kyocera Major incident'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'MFIcentral' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'MFIcentral'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'MFIcentral'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'MIS' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'MIS'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'MIS'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'MYQ' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'MYQ'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'MYQ'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'PM' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'PM'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'PM'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'Proprint' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'Proprint'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'Proprint'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'Proprint major incident' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'Proprint major incident'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'Proprint major incident'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        if 'Service Request' not in frappe.db.get_all('User Permission',{'allow':'Type of Call','user':doc.user_id},'for_value',pluck='for_value') or doc.user_id not in frappe.db.get_all('User Permission',{'allow':'Type of Call','for_value':'Service Request'},'user',pluck='user'):
+            usr_perm = frappe.new_doc('User Permission')
+            usr_perm.user = doc.user_id
+            usr_perm.allow = 'Type of Call'
+            usr_perm.for_value = 'Service Request'
+            usr_perm.apply_to_all_doctypes = 1
+            usr_perm.save()
+        
+        
 
 
 def get_roles_checked(doc,method):  
