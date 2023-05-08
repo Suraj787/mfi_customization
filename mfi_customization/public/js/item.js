ca5f75fc6f3c3ec7ee4f988d4a97233b417fd9ce
bizmapNikhil <nikhil.tulankar@bizmap.in>
2023-05-08 11:29:34 +0530

    [comment][search bar hide]

--- mfi_customization/public/js/item.js
+++ mfi_customization/public/js/item.js
@@ -1,10 +1,10 @@
 frappe.ui.form.on('Item', {
-    onload(frm) {
-        if(frappe.user.has_role("Customer")==1 || frappe.user.has_role("Technicians")==1 || frappe.user.has_role("Area Technical Manager")==1 && frappe.user!="Administrator"){
-            $(".form-control").hide();
-            $(".search-icon").hide();
-        }
-    },
+    // onload(frm) {
+    //     if(frappe.user.has_role("Customer")==1 || frappe.user.has_role("Technicians")==1 || frappe.user.has_role("Area Technical Manager")==1 && frappe.user!="Administrator"){
+    //         $(".form-control").hide();
+    //         $(".search-icon").hide();
+    //     }
+    // },
     setup:function(frm){
         frm.set_query("toner","compatible_toner_item", function() {
                 return {
