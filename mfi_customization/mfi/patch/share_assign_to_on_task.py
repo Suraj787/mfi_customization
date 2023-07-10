ef18aec2fdac8e940ad43b03b5de714e39e3afc4
hrishikesh-bizmap <120361653+hrishikesh-bizmap@users.noreply.github.com>
2023-07-10 06:31:21 +0000

    Update share_assign_to_on_task.py patch for issue DocShare

--- mfi_customization/mfi/patch/share_assign_to_on_task.py
+++ mfi_customization/mfi/patch/share_assign_to_on_task.py
@@ -9,7 +9,7 @@ import frappe
 def execute():
     # lst = []
     # for i in frappe.get_all("Task",['completed_by','name','assign_date','issue']):
-    for tk in  frappe.get_all('Task',['name','completed_by']):
+    for tk in  frappe.get_all('Task',['name','issue','completed_by']):
         if len(frappe.get_all('DocShare',{'user':tk.get('completed_by'),"share_doctype": 'Task',"share_name": tk.get('name')})) == 0: 
             docshare = frappe.new_doc("DocShare")
             docshare.update({
@@ -22,4 +22,16 @@ def execute():
                 
             docshare.save(ignore_permissions=True)
             print(tk.get('name')+" "+tk.get('completed_by')+" "+docshare.get('user'))
+        
+        if len(frappe.get_all('DocShare',{'user':tk.get('completed_by'),"share_doctype": 'Issue',"share_name": tk.get('issue')})) == 0: 
+            docshare = frappe.new_doc("DocShare")
+            docshare.update({
+                  "user": tk.get('completed_by'),
+                  "share_doctype": 'Issue',
+                  "share_name": tk.get('issue'),
+                  "read": 1,
+                  "write": 1
+            })
+                
+            docshare.save(ignore_permissions=True)
     
